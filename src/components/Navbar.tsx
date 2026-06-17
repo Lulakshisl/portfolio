1fb77ef7b7f16750883429ff2b457367e4e6c43e
Lulakshil <your-email@example.com>
2026-06-17 17:59:11 +0530

    Update navBar #2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -53,6 +53,7 @@ const Navbar = () => {
           cursor: 'pointer',
           display: 'flex', alignItems: 'center', justifyContent: 'center'
         }}>
+          
           {dark ? (
             <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#fbbf24" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
               <circle cx="12" cy="12" r="5"/>
