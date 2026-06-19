3eb581e85f3b47af3332ea0bb13d9211686d28b9
Lulakshil <your-email@example.com>
2026-06-19 22:19:16 +0530

    update navBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -170,9 +170,11 @@ const Navbar = () => {
                 <line x1="18.36" y1="5.64"  x2="19.78" y2="4.22"/>
               </svg>
             ) : (
+
               <svg width="16" height="16" viewBox="0 0 24 24" fill="#7c3aed" stroke="#7c3aed" strokeWidth="1">
                 <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/>
               </svg>
+              
             )}
           </button>
         </div>
